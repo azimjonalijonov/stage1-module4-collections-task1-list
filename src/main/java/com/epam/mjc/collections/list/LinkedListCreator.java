2b1bc490f650a083519d40ca516e0d3f828161fd
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2023-04-29 00:16:33 +0500

    Update LinkedListCreator.java

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,35 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> list = new LinkedList<>();
+        boolean tr1 = false;
+        boolean tr2 = false;
+        for (int i = 0; i < sourceList.size(); i++) {
+            if (sourceList.get(i) % 2 == 0) {
+                tr1 = true;
+            }
+            if (sourceList.get(i) % 2 != 0) {
+                tr2 = true;
+            }
+        }
+        if (tr1 && !tr2) {
+            list.addAll(sourceList);
+        } else if (!tr1 && tr2) {
+            for (int i = sourceList.size() - 1; i >= 0; i--) {
+                list.add(sourceList.get(i));
+            }
+        } else if (tr1 && tr2) {
+            for (int i = sourceList.size() - 1; i >= 0; i--) {
+                if (sourceList.get(i) % 2 != 0) {
+                    list.add(sourceList.get(i));
+                }
+            }
+            for (int i = 0; i < sourceList.size(); i++) {
+                if (sourceList.get(i) % 2 == 0) {
+                    list.add(sourceList.get(i));
+                }
+            }
+        }
+        return list;
     }
 }
